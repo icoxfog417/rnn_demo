7e92392356fa11a6330a78e8427b196f68b925b3
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-01-29 10:54:14 +0900

    fix ball count treatment

--- data/ball_data.py
+++ data/ball_data.py
@@ -10,57 +10,54 @@ http://nbviewer.ipython.org/github/cpbotha/bwtl-python-tutorials/blob/master/par
 """
 
 DIMENSION = 2
-VELOCITY_WEIGHT = np.array([10, 8])
+VELOCITY_WEIGHT = np.array([100, 80])
 TIME_INTERVAL = 0.1
 CoR = 0.95
 
 
-def bounce_ball(time=128, box_size=10, ball_count=1, initial_p=None, initial_v=None):
-    positions = []
-    velocities = []
+def bounce_balls(time=128, box_size=10, ball_count=1, initial_p=None, initial_v=None):
+    result = [bounce_ball(time, box_size, initial_p, initial_v) for x in range(ball_count)]
+    return result
+
+
+def bounce_ball(time=128, box_size=10, initial_p=None, initial_v=None):
+    features = ["positions", "velocities"]
+    result = np.zeros((time, len(features) * DIMENSION))
 
     p = initial_p
     if p is None:
-        p = gen_position(box_size, ball_count)
+        p = gen_position(box_size)
     else:
         p = np.copy(initial_p)
 
     v = initial_v
     if v is None:
-        v = gen_velocity(box_size, ball_count)
+        v = gen_velocity(box_size)
     else:
         v = np.copy(initial_v)
 
     t = 0
     while t < time:
-        positions.append([])
-        velocities.append([])
-        for i in range(ball_count):
-            positions[t].append(np.copy(p[i]))
-            velocities[t].append(np.copy(v[i]))
-            p[i], v[i] = __move(p[i], v[i], box_size)
-
+        result[t] = np.copy(np.hstack((p, v)))
+        p, v = __move(p, v, box_size)
         t += 1
 
-    return positions, velocities
+    return result
 
 
-def gen_position(box_size, ball_count):
-    p = []
-    for i in range(ball_count * DIMENSION):
-        p.append(random.uniform(0, box_size))
+def gen_position(box_size):
+    p = np.zeros(DIMENSION)
+    for i in range(DIMENSION):
+        p[i] = random.uniform(0, box_size)
+    return p
 
-    return np.array(p).reshape(ball_count, DIMENSION)
 
-
-def gen_velocity(box_size, ball_count):
-    v = []
-    # 2 means 2-dimension (position is expressed by x, y)
-    for i in range(ball_count * DIMENSION):
+def gen_velocity(box_size):
+    v = np.zeros(DIMENSION)
+    for i in range(DIMENSION):
         v_range = box_size / (VELOCITY_WEIGHT * TIME_INTERVAL)
-        v.append(random.uniform(v_range[0], v_range[1]))
-
-    return np.array(v).reshape(ball_count, DIMENSION)
+        v[i] = random.uniform(v_range[0], v_range[1])
+    return v
 
 
 def __move(p, v,  box_size):
@@ -82,29 +79,31 @@ def __move(p, v,  box_size):
 
     next_p = p + next_v * TIME_INTERVAL
     next_p = np.clip(next_p, 0, box_size)
+    if next_p[0] > box_size or next_p[1] > box_size:
+        print("error!")
 
     return next_p, next_v
 
 
 def show_animation(bounce_data, box_size, name=None):
-    if len(bounce_data) == 0:
-        return False
+    if not isinstance(bounce_data, list):
+        raise Exception("bounce_data has to be list of bounce ball")
 
     figure = plt.figure()
     area = figure.add_subplot(111, autoscale_on=False, xlim=box_size, ylim=box_size)
     area.grid()
     balls = []
-    for b in range(len(bounce_data[0])):
+    for b in range(len(bounce_data)):
         draw, = area.plot([], [], 'o', markersize=5)
         balls.append(draw)
 
     def animate(t):
-        ps = bounce_data[t]
-        for i, p in enumerate(ps):
+        for i in range(len(bounce_data)):
+            p = bounce_data[i][t][:2]
             balls[i].set_data(p[0], p[1])
         return balls
 
-    movie = animation.FuncAnimation(figure, animate, np.arange(len(bounce_data)), interval=100, blit=True)
+    movie = animation.FuncAnimation(figure, animate, bounce_data[0].shape[0], interval=100, blit=True)
     if name:
         try:
             movie.save("./{0}.gif".format(name), writer="imagemagick", fps=4)
@@ -119,5 +118,5 @@ if __name__ == "__main__":
     _time = 40
     _box_size = 10
     _ball_count = 1
-    data, _ = bounce_ball(_time, _box_size, _ball_count)
+    data = bounce_balls(_time, _box_size, _ball_count)
     show_animation(data, _box_size)
--- rnn_demo.py
+++ rnn_demo.py
@@ -9,19 +9,69 @@ from pybrain.supervised.trainers import BackpropTrainer
 import data.ball_data as ball_data
 
 
-BALL_COUNT = 1
 BOX_SIZE = 10
 
 
-def predict_ball(hidden_nodes, is_elman=True, training_data=5000, training_batch=-1, predict_count=64):
-    batch_size = training_data
-    if training_batch > 0:
-        if training_data < training_batch:
+def predict_ball(hidden_nodes, is_elman=True, training_data=5000, batch_size=-1, predict_count=64):
+    b_size = training_data
+    if batch_size > 0:
+        if training_data < batch_size:
             raise Exception("training count have to be greater than training batch size")
         else:
-            batch_size = training_batch
+            b_size = batch_size
 
     # build rnn
+    n = construct_network(hidden_nodes, is_elman)
+
+    # make training data
+    initial_v = ball_data.gen_velocity(BOX_SIZE)
+    data_set = None
+    training_ds = SupervisedDataSet(4, 4)
+
+    for b in range(training_data // b_size):
+        d = ball_data.bounce_ball(b_size + 1, BOX_SIZE, None, initial_v=initial_v)
+        data_set = d[:b_size] if data_set is None else np.vstack((data_set, d[:b_size]))
+
+        d_normalized = __normalize(d)
+        for i in range(b_size):
+            # from current, predict next
+            p_in = d_normalized[i].tolist()
+            p_out = d_normalized[i + 1].tolist()
+            training_ds.addSample(p_in, p_out)
+
+    total_avg = np.average(data_set, axis=0)
+    total_std = np.std(data_set, axis=0)
+    del data_set  # release memory
+
+    # training network
+    trainer = BackpropTrainer(n, training_ds)
+    err1 = trainer.train()
+    del training_ds  # release memory
+
+    # predict
+    initial_p = ball_data.gen_position(BOX_SIZE)
+    predict = None
+    next_pv = np.hstack((initial_p, initial_v))
+
+    n.reset()
+    for i in range(predict_count):
+        predict = next_pv if predict is None else np.vstack((predict, next_pv))
+
+        p_normalized = (next_pv - total_avg) / total_std
+        next_pv = n.activate(p_normalized.tolist())
+        restored = np.array(next_pv) * total_std + total_avg
+        next_pv = restored
+
+    real = ball_data.bounce_ball(predict_count, BOX_SIZE, initial_p, initial_v)
+    err_matrix = (predict - real) ** 2
+    err_distance = np.sqrt(np.sum(err_matrix[:, 0:2], axis=1)).reshape((predict_count, 1))
+    err_velocity = np.sum(np.sqrt(err_matrix[:, 2:4]), axis=1).reshape((predict_count, 1))
+    err2 = np.hstack((err_distance, err_velocity))
+
+    return predict, real, err1, err2
+
+
+def construct_network(hidden_nodes, is_elman=True):
     n = RecurrentNetwork()
     n.addInputModule(LinearLayer(4, name="i"))
     n.addModule(BiasUnit("b"))
@@ -41,88 +91,63 @@ def predict_ball(hidden_nodes, is_elman=True, training_data=5000, training_batch
         n.addRecurrentConnection(FullConnection(n["o"], n["h"]))
 
     n.sortModules()
-
-    # make training data
-    initial_v = ball_data.gen_velocity(BOX_SIZE, BALL_COUNT)
-    training_ds = SupervisedDataSet(4, 4)
-    p_all = []
-
-    for b in range(training_data // batch_size):
-        p_s, v_s = ball_data.bounce_ball(batch_size + 1, BOX_SIZE, BALL_COUNT, None, initial_v=initial_v)
-        p_all += p_s
-        p_s = __normalize(p_s)
-        v_s = __normalize(v_s)
-        for i in range(batch_size):
-            # from current, predict next
-            p_in = p_s[i][0].tolist() + v_s[i][0].tolist()
-            p_out = p_s[i + 1][0].tolist() + v_s[i + 1][0].tolist()
-            training_ds.addSample(p_in, p_out)
-
-    # training network
-    trainer = BackpropTrainer(n, training_ds)
-    err1 = trainer.train()
-
-    # predict
-    p_avg = np.average(np.array(p_all), axis=0)
-    p_std = np.std(np.array(p_all), axis=0)
-    initial_p = ball_data.gen_position(BOX_SIZE, BALL_COUNT)
-    predicts = []
-    next_pv = initial_p[0].tolist() + initial_v[0].tolist()
-    next_position = initial_p[0].tolist()
-
     n.reset()
-    for i in range(predict_count):
-        predicts.append([np.array(next_position)])
-        next_pv = n.activate(next_pv)
-        next_position = (np.array(next_pv[:2]) * p_std + p_avg).tolist()[0]
 
-    real_p, _ = ball_data.bounce_ball(predict_count, BOX_SIZE, BALL_COUNT, initial_p, initial_v)
-
-    err2 = __calc_diff(predicts, real_p)
-
-    return predicts, real_p, err1, err2
+    return n
 
 
-def __normalize(arr):
-    a = np.array(arr)
-    normalized = (a - np.average(a, axis=0)) / np.std(a, axis=0)
+def __normalize(data):
+    normalized = (data - np.average(data, axis=0)) / np.std(data, axis=0)
     return normalized
 
 
-def __calc_diff( predicts, results):
-    diffs = []
-    for i, v in enumerate(results):
-        diff = 0
-        for j, b in enumerate(results[i]):
-            diff += np.sqrt(np.sum((predicts[i][j] - results[i][j]) ** 2))
-        diffs.append(diff)
-
-    return np.mean(diffs)
+def describe_err(error, separator=","):
+    params = np.hstack((np.mean(error, axis=0), np.std(error, axis=0)))
+    return separator.join(["{0}".format(p) for p in params])
 
 
-def eval_model(min_hidden, max_hidden, is_elman=True, step=10, training_data=5000, trial_run=10):
+def measure_hidden_effect(min_hidden, max_hidden, is_elman=True, step=10, training_data=5000, trial_run=10):
     for h in range(min_hidden, max_hidden + step, step):
         training_e = []
-        test_e = []
+        test_e = None
 
         for i in range(trial_run):
             p, r, e1, e2 = predict_ball(h, is_elman, training_data)
-            print("{0}\t{1}\t{2}".format(h, e1, e2))
             training_e.append(e1)
-            test_e.append(e2)
+            test_e = e2 if test_e is None else np.vstack((test_e, e2))
 
-        print("{0}\t{1}\t{2}".format(h, median(training_e), median(test_e)))
+        print("{0}\t{1}\t{2}".format(h, median(training_e), describe_err(test_e, "\t")))
+
+
+def measure_batch_effect(hidden_nodes, min_size, max_size, is_elman=True, step=100, training_data=20000, trial_run=10):
+    for b in range(min_size, max_size + step, step):
+        training_e = []
+        test_e = None
+
+        for i in range(trial_run):
+            p, r, e1, e2 = predict_ball(hidden_nodes, is_elman, training_data, b)
+            training_e.append(e1)
+            test_e = e2 if test_e is None else np.vstack((test_e, e2))
+
+        print("{0}\t{1}\t{2}".format(b, median(training_e), describe_err(test_e, "\t")))
+
+
+def run(is_elman=True):
+    nodes = 4
+    p, r, e1, e2 = predict_ball(nodes, is_elman=is_elman, training_data=64000, batch_size=64)
+    print("training error:{0}, test error:{1}".format(e1, describe_err(e2)))
+    for x in p:
+        print("{0}, {1}".format(x[0], x[1]))
+
+    ball_data.show_animation([r], BOX_SIZE + 1)
+    ball_data.show_animation([p], BOX_SIZE + 1)
 
 
 def main(is_elman=True):
     # evaluate model by changing hidden layer
-    # eval_model(4, 20, True, step=1)
+    # measure_batch_effect(4, 64, 2048, False, step=64)
 
-    nodes = 4
-    p, r, e1, e2 = predict_ball(nodes, is_elman=is_elman, training_data=32000, training_batch=1280)
-    print("training error:{0}, test error:{1}".format(e1, e2))
-    ball_data.show_animation(r, BOX_SIZE)
-    ball_data.show_animation(p, BOX_SIZE)
+    run(is_elman)
 
 
 if __name__ == "__main__":
